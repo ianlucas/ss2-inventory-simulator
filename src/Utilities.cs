d3300138148eb2914bf9f97590c4287770135588
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-12-09 19:10:09 -0300

    Format URL before printing it

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -3,13 +3,37 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using System.Reflection;
+using System.Text.RegularExpressions;
 using SwiftlyS2.Shared;
 using SwiftlyS2.Shared.Players;
 
-public static class Utilities
+public static partial class Utilities
 {
     public static IPlayer? GetPlayerFromSteamID(ISwiftlyCore core, ulong steamId)
     {
         return core.PlayerManager.GetAllPlayers().FirstOrDefault(p => p.SteamID == steamId);
     }
+
+    public static string FormatUrl(string format, string urlString)
+    {
+        if (!Uri.TryCreate(urlString, UriKind.Absolute, out var uri))
+            return format;
+        return UrlFormatPattern()
+            .Replace(
+                format,
+                match =>
+                {
+                    var prop = uri.GetType()
+                        .GetProperty(
+                            match.Groups[1].Value,
+                            BindingFlags.Public | BindingFlags.Instance
+                        );
+                    return prop?.GetValue(uri)?.ToString() ?? match.Value;
+                }
+            );
+    }
+
+    [GeneratedRegex(@"\{(\w+)\}")]
+    private static partial Regex UrlFormatPattern();
 }
